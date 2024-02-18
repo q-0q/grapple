8d24ea0420f618a1311524e7479217a75e6f10bd
Jack Withers <jackwithers@Jacks-MacBook-Pro.local>
2024-02-17 20:15:33 -0500

    grappling.

--- Scripts/Player/AirGrappleFire.gd
+++ Scripts/Player/AirGrappleFire.gd
@@ -25,7 +25,7 @@ func on_enter():
 		compute_attach_point()
 		
 		character().get_node("AnimationPlayer").play("air_grapple_fire")
-		lock_state_for_duration(0.75)
+		lock_state_for_duration(0.7)
 		
 		$AnimationPlayer.play("fire")
 		$holder.position = FiringPoint.global_position
@@ -34,12 +34,12 @@ func on_enter():
 		line.add_point(ExtensionPoint.global_position)
 		line.add_point(attach_point)
 		
-		Util.delayed_callback(self._on_timeout, 0.75)
+		Util.delayed_callback(self._on_timeout, 0.7)
 	
 func on_process():
 		line.points[0] = ExtensionPoint.global_position
 		$holder.position = FiringPoint.global_position
-		# $holder.rotation_degrees = compute_holder_rotation_deg()
+		$holder.rotation_degrees = compute_holder_rotation_deg()
 		character().velocity = character().velocity.lerp(Vector2.ZERO, 20 * get_process_delta_time())
 	
 func on_exit():
@@ -50,7 +50,7 @@ func condition():
 	return Input.is_action_just_released("Grapple")
 	
 func compute_holder_rotation_deg() -> float:
-	return rad_to_deg(Vector2.DOWN.angle_to( FiringPoint.global_position - get_global_mouse_position()))
+	return rad_to_deg(Vector2.DOWN.angle_to( FiringPoint.global_position - attach_point))
 
 func compute_attach_point() -> void:
 	attach_point = get_local_mouse_position()
